787b077a73624b4a2fac1e2a5c99a383f92a5543
Piyush Keshari <piyush.keshari@springworks.in>
2024-01-10 19:36:06 +0530

    converted to react app

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown, faCaretRight, faIndianRupeeSign } from '@fortawesome/free-solid-svg-icons';
+
+function App() {
+  const Menu = require('./data/menu.json');
+  const [showMenu, setShowMenu] = useState(Object.fromEntries(Object.keys(Menu).map(category => [category, false])));
+
+  const toggleMenu = (category) => {
+    setShowMenu({ ...showMenu, [category]: !showMenu[category] });
+  }
+
+  return (
+    <div className="menu-container">
+      <h1 className='text-5xl menu-title'>New Rajshree Sweets Menu</h1>
+      {Object.keys(Menu).map((category) => (
+        <React.Fragment key={category}>
+          <div className="category-container">
+            <div className={`bg-rose-600 hover:bg-rose-700 text-white category-title-wrapper ${showMenu[category] ? 'active' : ''}`} onClick={() => toggleMenu(category)}>
+              <h1 className='text-2xl category-title'>{category}</h1>
+              <FontAwesomeIcon icon={showMenu[category] ? faCaretDown : faCaretRight} className='caret' />
+            </div>
+            <div className="items-container">
+              {Menu[category].map((item, index) => (
+                <div key={index} className={`item-wrapper ${index % 2 === 0 ? 'alternate' : ''}`} >
+                  <span className='item-title'>{item.name}</span>
+                  <span className='item-rate'><FontAwesomeIcon icon={faIndianRupeeSign} /> {item.rate}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
+
+export default App;
