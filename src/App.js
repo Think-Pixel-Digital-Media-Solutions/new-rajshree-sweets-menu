650c21dae9076241e6c371913012a2c4f6f1d544
Piyush Keshari <piyush.keshari@springworks.in>
2024-01-12 19:40:37 +0530

    changed styling

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ function App() {
 
   return (
     <div className="menu-container">
-      <h1 className='text-3xl font-bold menu-title'>NEW RAJSHREE SWEETS PRIVATE LIMITED</h1>
-      <p className='text-2xl font-bold'>S 6/109-110 ORDERLY BAZAR ROAD</p>
-      <p className='text-2xl font-bold'>GOLGHAR KACHAHARI,VARANASI-221002</p>
-      <p className='text-xl mt-5'><span className='font-bold'>FSSAI LIC NO:</span> 12714038000517</p>
-      <p className='text-xl'><span className='font-bold'>Phone:</span> 0542-2504477</p>
-      <p className='text-xl'><span className='font-bold'>Email:</span> newrajshreesweets@yahoo.in</p>
+      <h1 className='text-2xl font-bold menu-title mb-3'>NEW RAJSHREE SWEETS PRIVATE LIMITED</h1>
+      <p className='text-xl font-bold'>S 6/109-110 ORDERLY BAZAR ROAD</p>
+      <p className='text-xl font-bold'>GOLGHAR KACHAHARI,VARANASI-221002</p>
+      <p className='text-l mt-5'><span className='font-bold'>FSSAI LIC NO:</span> 12714038000517</p>
+      <p className='text-l'><span className='font-bold'>Phone:</span> 0542-2504477</p>
+      <p className='text-l'><span className='font-bold'>Email:</span> newrajshreesweets@yahoo.in</p>
       {Object.keys(Menu).map((category) => (
         <React.Fragment key={category}>
           <div className="category-container">
