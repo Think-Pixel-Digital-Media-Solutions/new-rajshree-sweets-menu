2088d39d5fb20f125c066d6bcfb345ccbcfd65b8
Piyush Keshari <piyush.keshari@springworks.in>
2024-01-15 20:51:53 +0530

    added space before colon

--- src/App.js
+++ src/App.js
@@ -18,11 +18,11 @@ function App() {
         <p className='text-xl font-bold'>S 6/109-110 ORDERLY BAZAR ROAD</p>
         <p className='text-xl font-bold'>GOLGHAR KACHAHARI</p>
         <p className='text-xl font-bold mb-5'>VARANASI - 221002</p>
-        <p className='text-l'><span className='font-bold'>CIN:</span> U15490UP2021PTC156096</p>
-        <p className='text-l'><span className='font-bold'>GSTIN:</span> 09AAHCN9500A1ZP</p>
-        <p className='text-l'><span className='font-bold'>FSSAI LIC NO:</span> 12714038000517</p>
-        <p className='text-l'><span className='font-bold'>Phone:</span> 0542-2504477</p>
-        <p className='text-l'><span className='font-bold'>Email:</span> newrajshreesweets@yahoo.in</p>
+        <p className='text-l'><span className='font-bold'>CIN :</span> U15490UP2021PTC156096</p>
+        <p className='text-l'><span className='font-bold'>GSTIN :</span> 09AAHCN9500A1ZP</p>
+        <p className='text-l'><span className='font-bold'>FSSAI LIC NO :</span> 12714038000517</p>
+        <p className='text-l'><span className='font-bold'>Phone :</span> 0542-2504477</p>
+        <p className='text-l'><span className='font-bold'>Email :</span> newrajshreesweets@yahoo.in</p>
       </div>
       {Object.keys(Menu).map((category) => (
         <React.Fragment key={category}>
