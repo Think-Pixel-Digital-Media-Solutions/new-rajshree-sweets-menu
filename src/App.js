951cb9972429683394e0a538dc19aaf745bd6199
Piyush Keshari <piyush.keshari@springworks.in>
2024-01-15 16:54:19 +0530

    modified layout

--- src/App.js
+++ src/App.js
@@ -13,10 +13,12 @@ function App() {
   return (
     <div className="menu-container">
       <div className="misc-details">
+        <img src={require('./assets/logo.png')} alt='logo' className='logo' />
         <h1 className='text-2xl font-bold menu-title mb-3'>NEW RAJSHREE SWEETS PRIVATE LIMITED</h1>
         <p className='text-xl font-bold'>S 6/109-110 ORDERLY BAZAR ROAD</p>
         <p className='text-xl font-bold'>GOLGHAR KACHAHARI</p>
         <p className='text-xl font-bold mb-5'>VARANASI - 221002</p>
+        <p className='text-l'><span className='font-bold'>CIN:</span> U15490UP2021PTC156096</p>
         <p className='text-l'><span className='font-bold'>GSTIN:</span> 09AAHCN9500A1ZP</p>
         <p className='text-l'><span className='font-bold'>FSSAI LIC NO:</span> 12714038000517</p>
         <p className='text-l'><span className='font-bold'>Phone:</span> 0542-2504477</p>
